138493986927c9ccc87bf6deeee19071029d8b03
alluShubham <shubhamaryan857@gmail.com>
2022-05-08 15:40:54 +0530

    updated
    seprate

---
+++ script/fragnance.js
@@ -0,0 +1,36 @@
+function ShowAndHide() {
+    var x = document.getElementById("SectionName");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+  
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide2() {
+    var x = document.getElementById("SectionName2");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide3() {
+    var x = document.getElementById("SectionName3");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide4() {
+    var x = document.getElementById("SectionName4");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  } 
---
+++ script/makeup.js
@@ -0,0 +1,63 @@
+function ShowAndHide() {
+    var x = document.getElementById("SectionName");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+  
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide2() {
+    var x = document.getElementById("SectionName2");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide3() {
+    var x = document.getElementById("SectionName3");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide4() {
+    var x = document.getElementById("SectionName4");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide5() {
+    var x = document.getElementById("SectionName5");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide6() {
+    var x = document.getElementById("SectionName6");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ShowAndHide7() {
+    var x = document.getElementById("SectionName7");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
